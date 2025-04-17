c684e6c5789448286036427b5cdc80c36b4e6cfd
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2025-04-17 11:13:29 -0500

    Initial config

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+    const logger = new Logger('Main');
+
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+
+    await app.listen(4000);
+}
+bootstrap();
