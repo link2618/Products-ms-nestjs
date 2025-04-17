c9d04a4ca6b9319f5e50142be83b3650b864eccd
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2025-04-17 12:37:30 -0500

    Created crud with http request

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,10 +6,13 @@ import {
     Patch,
     Param,
     Delete,
+    Query,
+    ParseIntPipe,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common';
 
 @Controller('products')
 export class ProductsController {
@@ -21,8 +24,8 @@ export class ProductsController {
     }
 
     @Get()
-    findAll() {
-        return this.productsService.findAll();
+    findAll(@Query() paginationDto: PaginationDto) {
+        return this.productsService.findAll(paginationDto);
     }
 
     @Get(':id')
@@ -32,10 +35,11 @@ export class ProductsController {
 
     @Patch(':id')
     update(
-        @Param('id') id: string,
+        @Param('id', ParseIntPipe) id: number,
         @Body() updateProductDto: UpdateProductDto,
     ) {
-        return this.productsService.update(+id, updateProductDto);
+        console.log('pasa');
+        return this.productsService.update(id, updateProductDto);
     }
 
     @Delete(':id')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+    Injectable,
+    Logger,
+    NotFoundException,
+    OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -14,22 +20,73 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     }
 
     create(createProductDto: CreateProductDto) {
-        return 'This action adds a new product';
+        return this.product.create({
+            data: createProductDto,
+        });
     }
 
-    findAll() {
-        return `This action returns all products`;
+    async findAll(paginationDto: PaginationDto) {
+        const { page = 1, limit = 10 } = paginationDto;
+
+        const totalRecords = await this.product.count({
+            where: { available: true },
+        });
+        const lastPage = Math.ceil(totalRecords / limit);
+
+        return {
+            data: await this.product.findMany({
+                skip: (page - 1) * limit,
+                take: limit,
+                where: {
+                    available: true,
+                },
+            }),
+            meta: {
+                total: totalRecords,
+                page: page,
+                lastPage: lastPage,
+            },
+        };
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} product`;
+    async findOne(id: number) {
+        const product = await this.product.findFirst({
+            where: { id, available: true },
+        });
+
+        if (!product) {
+            throw new NotFoundException(`Product with id #${id} not found`);
+        }
+
+        return product;
     }
 
-    update(id: number, updateProductDto: UpdateProductDto) {
-        return `This action updates a #${id} product`;
+    async update(id: number, updateProductDto: UpdateProductDto) {
+        // const { id: __, ...data } = updateProductDto;
+
+        await this.findOne(id);
+
+        return this.product.update({
+            where: { id },
+            // data: data,
+            data: updateProductDto,
+        });
     }
 
-    remove(id: number) {
-        return `This action removes a #${id} product`;
+    async remove(id: number) {
+        await this.findOne(id);
+
+        // return this.product.delete({
+        //   where: { id }
+        // });
+
+        const product = await this.product.update({
+            where: { id },
+            data: {
+                available: false,
+            },
+        });
+
+        return product;
     }
 }
