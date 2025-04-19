5051bf5d5c8423e67005fe18d661ce75c4de0659
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2025-04-19 16:12:10 -0500

    Change tcp to nats

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,9 @@ async function bootstrap() {
     const app = await NestFactory.createMicroservice<MicroserviceOptions>(
         AppModule,
         {
-            transport: Transport.TCP,
+            transport: Transport.NATS,
             options: {
-                port: envs.port,
+                servers: envs.natsServers,
             },
         },
     );
